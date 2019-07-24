8e38bdec672d63a2d3a072d72317c83fa735e0ce
santhosh274 <santhosh.k@rapyder.com>
2019-07-24 22:04:33 +0530

    Extracting options to config object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer : {
+        text : "let's hit the beach!",
+        iconName : 'sun'
+    },
+    winter : {
+        text : 'Burr its Cold',
+        iconName : 'snowwflake'
+    }
+}
+
 const getSeason = (lat , month) =>{
     if(month > 2 && month < 9){
       return  lat > 0 ? 'summer' : 'winter';
@@ -11,15 +22,14 @@ const getSeason = (lat , month) =>{
 
 const SeasonDisplay = (props) =>{
     const season = getSeason(props.latitude,new Date().getMonth());
-    const text = season === 'winter' ? 'Burr , it is Chilly' : 'Lets hit the beach';
-    const icon = season === 'winter'? 'snowflake' : 'sun' 
+     const {text , iconName} =seasonConfig[season] //{ text , iconName }
 
     console.log(season);
     return(
         <div>
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
         <h1>{text}</h1>
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
         </div>
     );
 };
