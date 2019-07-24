adfc3fb9b5ee5bf9c1ef71fabe82f4efb4e04d66
santhosh274 <santhosh.k@rapyder.com>
2019-07-24 21:40:47 +0530

    Show text based on the Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+const getSeason = (lat , month) =>{
+    if(month > 2 && month < 9){
+      return  lat > 0 ? 'summer' : 'winter';
+    }
+    else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
 const SeasonDisplay = (props) =>{
-    console.log(props.latitude);
+    const season = getSeason(props.latitude,new Date().getMonth());
+    const text = season === 'winter' ? 'Burr , it is Chilly' : 'Lets hit the beach';
+
+    console.log(season);
     return(
-        <div>Season Display</div>
+        <div>
+        <h1>{text}</h1>
+        </div>
     );
 };
 
