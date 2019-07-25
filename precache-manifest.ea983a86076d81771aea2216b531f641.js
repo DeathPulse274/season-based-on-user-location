afd4c59a99817498e1ca51632920beacb36ca586
santhosh274 <santhosh.k@rapyder.com>
2019-07-25 10:12:05 +0530

    Updates

---
+++ precache-manifest.ea983a86076d81771aea2216b531f641.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320263e3f7284777c8f9c503bc4b27df",
+    "url": "/season-based-on-user-location/index.html"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/season-based-on-user-location/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "e0ba7762d323de683026",
+    "url": "/season-based-on-user-location/static/js/main.8b5c469f.chunk.js"
+  },
+  {
+    "revision": "30ae06b890168aee6933",
+    "url": "/season-based-on-user-location/static/js/runtime~main.725bf245.js"
+  }
+]);
