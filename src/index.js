9a2f738468fa888a7a6ba4f2bf9e7cef99a45ebf
santhosh274 <santhosh.k@rapyder.com>
2019-07-25 18:06:47 +0530

    default props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 class App extends React.Component{
     
@@ -30,7 +31,7 @@ class App extends React.Component{
             )
         }
 
-        return <div>Loading!</div>
+        return <Spinner message="Please Accept Location Request"></Spinner>
     }
 }
 
