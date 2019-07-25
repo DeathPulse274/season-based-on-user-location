8144e125f4504915a7ca3c78c9c90d2c7c49fd33
santhosh274 <santhosh.k@rapyder.com>
2019-07-25 17:46:04 +0530

    added new css file

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css'
 import React from 'react';
 
 //Can avoid Multple Ternary Operator by using season Config
@@ -26,13 +27,11 @@ const SeasonDisplay = (props) =>{
 
     //acts as ternari operator which takes input as season and returns appropriate value getSeason () => String shouuld be same [summer,winter]
      const {text , iconName} =seasonConfig[season] //{ text , iconName }
-
-    console.log(season);
     return(
-        <div>
-            <i className={`${iconName} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
         <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 };
